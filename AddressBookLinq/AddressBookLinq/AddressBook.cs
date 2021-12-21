59a7841b295750aa69e27fef12ee946df6f994ae
shital bramhe <shitalbramhe@gmail.com>
2021-12-21 18:20:53 +0530

    [shital] ADD : Added Contact

--- AddressBookLinq/AddressBookLinq/AddressBook.cs
+++ AddressBookLinq/AddressBookLinq/AddressBook.cs
@@ -10,7 +10,7 @@ namespace AddressBookLinq
     public class AddressBook
     {
         DataTable datatable = new DataTable();
-        public void AddDataTable()
+        public void AddDataTable(List<Contact> contactlist)
         {
             datatable.Columns.Add("firstname");
             datatable.Columns.Add("lastname");
@@ -18,8 +18,17 @@ namespace AddressBookLinq
             datatable.Columns.Add("city");
             datatable.Columns.Add("state");
             datatable.Columns.Add("zip").DataType = typeof(Int32);
-            datatable.Columns.Add("phonenumber").DataType = typeof(Int32);
-            datatable.Columns.Add("email"); 
+            datatable.Columns.Add("phonenumber").DataType = typeof(Int64);
+            datatable.Columns.Add("email");
+            foreach (var item in contactlist)
+            {
+                datatable.Rows.Add(item.firstname, item.lastname, item.address, item.city, item.state, item.zip, item.phonenumber, item.email);
+            }
+            var AddressTable = from data in datatable.AsEnumerable() select data;
+            foreach (DataRow item in AddressTable)
+            {
+                Console.WriteLine(item.Field<string>("firstname") + " " + item.Field<string>("lastname") + " " + item.Field<string>("address") + " " + item.Field<string>("city") + " " + item.Field<string>("state") + " " + item.Field<int>("zip") + " " + item.Field<long>("phonenumber") + " " + item.Field<string>("email"));
+            }
         }
     }
 }
--- AddressBookLinq/AddressBookLinq/Program.cs
+++ AddressBookLinq/AddressBookLinq/Program.cs
@@ -12,7 +12,15 @@ namespace AddressBookLinq
         {
             Console.WriteLine("Address book LINQ problem");
             AddressBook address = new AddressBook();
-
+            List<Contact> contactlist = new List<Contact>()
+            {
+                new Contact{ firstname = "Gita", lastname = "Pande", address = "Nandavan", city = "Nagpur", state = "Maharashtra", zip = 440076 ,phonenumber = 1234567890,email = "Gita@gmail.com" },
+                new Contact{ firstname = "Sita", lastname = "Shende", address = "Itwari", city = "Wardha", state = "Maharashtra", zip = 440046 ,phonenumber = 6784567890,email = "Sita@gmail.com" },
+                new Contact{ firstname = "Meena", lastname = "Dhore", address = "MahalgiNagar", city = "Nagpur", state = "Maharashtra", zip = 440026 ,phonenumber = 7834567890,email = "Mina@gmail.com" },
+                new Contact{ firstname = "Tina", lastname = "Bele", address = "SanjayGandhi", city = "Yavatmal", state = "Maharashtra", zip = 440054 ,phonenumber = 9234567890,email = "Teena@gmail.com" },
+                new Contact{ firstname = "Ashu", lastname = "Thakre", address = "New Nandavan", city = "Nagpur", state = "Maharashtra", zip = 440032 ,phonenumber = 8234567890,email = "Ashu@gmail.com" }
+            };
+            address.AddDataTable(contactlist);
         }
     }
 }
