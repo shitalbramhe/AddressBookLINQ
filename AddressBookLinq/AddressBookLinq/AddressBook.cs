fcb7eed3870a5fc4d95f94d093fb1ca3e3a8c5c8
shital bramhe <shitalbramhe@gmail.com>
2021-12-21 16:19:03 +0530

    [shital] ADD : Added Attribute

--- AddressBookLinq/AddressBookLinq/AddressBook.cs
+++ AddressBookLinq/AddressBookLinq/AddressBook.cs
@@ -9,9 +9,17 @@ namespace AddressBookLinq
 {
     public class AddressBook
     {
+        DataTable datatable = new DataTable();
         public void AddDataTable()
         {
-            DataTable datatable = new DataTable();
+            datatable.Columns.Add("firstname");
+            datatable.Columns.Add("lastname");
+            datatable.Columns.Add("address");
+            datatable.Columns.Add("city");
+            datatable.Columns.Add("state");
+            datatable.Columns.Add("zip").DataType = typeof(Int32);
+            datatable.Columns.Add("phonenumber").DataType = typeof(Int32);
+            datatable.Columns.Add("email"); 
         }
     }
 }
