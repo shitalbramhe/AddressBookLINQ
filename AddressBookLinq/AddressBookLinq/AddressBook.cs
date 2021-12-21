731143997ff08337b28569d688a07ff6744a01d9
shital bramhe <shitalbramhe@gmail.com>
2021-12-21 16:02:34 +0530

    [shital] ADD : initial commit

---
+++ AddressBookLinq/AddressBookLinq/AddressBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookLinq
+{
+    public class AddressBook
+    {
+        public void AddDataTable()
+        {
+            DataTable datatable = new DataTable();
+        }
+    }
+}
---
+++ AddressBookLinq/AddressBookLinq/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Address book LINQ problem");
+            AddressBook address = new AddressBook();
+
+        }
+    }
+}
